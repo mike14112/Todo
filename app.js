09422801f61b91d7c1fbde3733a52d12a6f61e81
Mike <mixailted@gmail.com>
2022-10-20 17:13:13 -0700

    add  new fiches

--- app.js
+++ app.js
@@ -25,8 +25,26 @@ const checkingComplete = function () {
   }
 };
 
+document.addEventListener("keydown", (e) => {
+  console.log();
+  if (
+    e.key === "Enter" &&
+    inputTodo.value != "" &&
+    inputTodo.value.length <= 25
+  ) {
+    list.innerHTML += `<ul><li class ="task-list">  <div><input type="checkbox" class = "checkbox"></div>
+    <div><span>${inputTodo.value} </span></div>
+  <div> <button class ="btn-remove">x</button></div></li> </ul>`;
+    checkingComplete();
+  }
+});
+
 btnAdd.addEventListener("click", () => {
-  if (inputTodo.value && inputTodo.value.length <= 25) {
+  if (
+    inputTodo.value &&
+    inputTodo.value.length <= 25 &&
+    inputTodo.value != ""
+  ) {
     list.innerHTML += `<ul><li class ="task-list">  <div><input type="checkbox" class = "checkbox"></div>
                         <div><span>${inputTodo.value} </span></div>
                       <div> <button class ="btn-remove">x</button></div></li> </ul>`;
