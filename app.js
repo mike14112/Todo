6a07433cbd6378b520f7f334fd102ad4892613e5
Mike <89590104+mike14112@users.noreply.github.com>
2023-05-02 01:44:07 +0400

    new Todo
     მთლიანად გადაიწერა  Projecti  სადაც გამოყენებულია ახალი ტექნოლოგიები  და ახალი მიდგომა

--- app.js
+++ app.js
@@ -1,55 +1,28 @@
-const btnAdd = document.querySelector(".btn-add"),
-  inputTodo = document.querySelector(".input-todo"),
-  list = document.querySelector(".list");
+const App = {
+    data(){
+        return {
+            title:'თქვენი შენიშვნები',
+            notes:[],
+            inputValue: '',
+            
+        }
+    
+    },
 
-const checkingComplete = function () {
-  var taskAll = document.querySelectorAll("span"),
-    taskComplete = document.querySelectorAll("li"),
-    checkboxAll = document.querySelectorAll(".checkbox"),
-    btnTaskRemove = document.querySelectorAll(".btn-remove");
+    methods: {
 
-  for (let i = 0; i < taskAll.length; i++) {
-    checkboxAll[i].addEventListener("change", (event) => {
-      if (event.currentTarget.checked) {
-        taskAll[i].classList.add("complete");
-      } else {
-        taskAll[i].classList.remove("complete");
+        addNewNote(){
+        if(this.inputValue !== ''){
+            this.notes.push(this.inputValue)
+        }
+     
+        }  ,
+        deleteNote(index){
+            this.notes.splice(index, 1)
+            this.inputValue = ''
+        }
       }
-    });
-  }
 
-  for (let i = 0; i < taskAll.length; i++) {
-    btnTaskRemove[i].addEventListener("click", () => {
-      taskComplete[i].style.display = "none";
-    });
-  }
-};
+}
 
-document.addEventListener("keydown", (e) => {
-  console.log();
-  if (
-    e.key === "Enter" &&
-    inputTodo.value != "" &&
-    inputTodo.value.length <= 25
-  ) {
-    list.innerHTML += `<ul><li class ="task-list">  <div><input type="checkbox" class = "checkbox"></div>
-    <div><span>${inputTodo.value} </span></div>
-  <div> <button class ="btn-remove">x</button></div></li> </ul>`;
-  inputTodo.value  = ''
-    checkingComplete();
-  }
-});
-
-btnAdd.addEventListener("click", () => {
-  if (
-    inputTodo.value &&
-    inputTodo.value.length <= 25 &&
-    inputTodo.value != ""
-  ) {
-    list.innerHTML += `<ul><li class ="task-list">  <div><input type="checkbox" class = "checkbox"></div>
-                        <div><span>${inputTodo.value} </span></div>
-                      <div> <button class ="btn-remove">x</button></div></li> </ul>`;
-    inputTodo.value = "";
-    checkingComplete();
-  }
-});
+Vue.createApp(App).mount('#app')
